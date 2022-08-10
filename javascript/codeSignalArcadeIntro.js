7f26b78a4a821858208c948dfcbaa4c5de2e05ad
davidpierce24 <pie2ce@gmail.com>
2022-08-10 14:17:10 -0400

    updates

--- javascript/codeSignalArcadeIntro.js
+++ javascript/codeSignalArcadeIntro.js
@@ -22,3 +22,20 @@
 //     }
 //     return true
 // }
+
+// 4 adjacentElementsProduct
+// function solution(inputArray) {
+//     let product = inputArray[0]*inputArray[1]
+//     for(let i = 1; i < inputArray.length-1; i++){
+//         if(inputArray[i] * inputArray[i+1] > product){
+//             product = inputArray[i] * inputArray[i+1]
+//         }
+//     }
+//     return product
+// }
+
+// 5 shapeArea
+// function solution(n) {
+//     return ((((n*2)-1)*n)-(n-1))
+// }
+

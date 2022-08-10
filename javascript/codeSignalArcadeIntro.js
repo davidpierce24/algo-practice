4131d8dd8c3275c5711f13a962c2ad0b0da00b3b
davidpierce24 <pie2ce@gmail.com>
2022-08-10 14:15:56 -0400

    code signal intro

---
+++ javascript/codeSignalArcadeIntro.js
@@ -0,0 +1,24 @@
+// 1 add
+// function solution(param1, param2) {
+//     return param1 + param2
+// }
+
+
+// 2 centuryFromYear
+// function solution(year) {
+//     return Math.ceil(year/100)
+// }
+
+// 3 checkPalindrome
+// function solution(inputString) {
+//     if(inputString.length == 1){
+//         return true
+//     }
+//     for(let i = 0, j = inputString.length-1; i < inputString.length, j >=0; i++, j--){
+//         if(inputString[i] == inputString[j]){
+//             continue
+//         }
+//         return false
+//     }
+//     return true
+// }

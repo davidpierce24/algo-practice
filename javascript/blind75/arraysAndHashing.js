8137d476cae78c8bc6042b6745697a04ac6be9be
davidpierce24 <pie2ce@gmail.com>
2022-08-16 11:11:58 -0400

    updates

--- javascript/blind75/arraysAndHashing.js
+++ javascript/blind75/arraysAndHashing.js
@@ -68,18 +68,3 @@
         return true
     };
 
-
-// Best Time to Buy and Sell Stock
-    var maxProfit = function(prices) {
-        var profit = 0
-        var min = prices[0]
-        for(var i of prices){
-            if(i - min > profit){
-                profit = i - min
-            }
-            if(i < min){
-                min  = i
-            }
-        }
-        return profit
-    };
--- javascript/blind75/stack.js
+++ javascript/blind75/stack.js
@@ -1,18 +1,18 @@
 // Valid Parentheses
-var isValid = function(s) {
-    if(s.length%2 != 0){
-        return false
-    }
-    var stack = []
-    
-    for(var i of s){
-        if(stack[stack.length-1] == '{' && i == '}' || stack[stack.length-1] == '(' && i == ')' || stack[stack.length-1] == '[' && i == ']'){
-            stack.pop()
+    var isValid = function(s) {
+        if(s.length%2 != 0){
+            return false
         }
-        else{
-            stack.push(i)
-        }
-    }
+        var stack = []
         
-    return stack.length == 0
-};
+        for(var i of s){
+            if(stack[stack.length-1] == '{' && i == '}' || stack[stack.length-1] == '(' && i == ')' || stack[stack.length-1] == '[' && i == ']'){
+                stack.pop()
+            }
+            else{
+                stack.push(i)
+            }
+        }
+            
+        return stack.length == 0
+    };

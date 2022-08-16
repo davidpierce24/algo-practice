73c42fbdacbb9bbfc5f2b9a9becf42a169207c20
davidpierce24 <pie2ce@gmail.com>
2022-08-16 09:16:24 -0400

    updates

---
+++ javascript/blind75/arraysAndHashing.js
@@ -0,0 +1,12 @@
+// Contains Duplicate
+var containsDuplicate = function(nums) {
+    let map = {}
+    for(let i of nums){
+        if(i in map){
+            return true
+        } else {
+            map[i] = 1
+        }
+    }
+    return false
+};

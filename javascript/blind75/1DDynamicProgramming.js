d17685b3a06b1d2e760668450c30ef2ba17d2f0b
davidpierce24 <pie2ce@gmail.com>
2022-08-16 11:59:41 -0400

    updates

--- javascript/blind75/1DDynamicProgramming.js
+++ javascript/blind75/1DDynamicProgramming.js
@@ -6,3 +6,15 @@
         }
         return(fib[fib.length-1])
     };
+
+    // neetcode version
+    var climbStairs = function(n) {
+        let dp1 = 1;
+        let dp2 = 1;
+        for(let i=2; i<=n; i++) {
+            const temp = dp2;
+            dp2 = dp2 + dp1;
+            dp1 = temp;
+        }
+        return dp2;
+    };

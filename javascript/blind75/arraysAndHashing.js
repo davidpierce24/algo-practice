e530c60fa60be0331efa43b83a9d46217e32c61e
davidpierce24 <pie2ce@gmail.com>
2022-08-16 10:20:54 -0400

    updates

--- javascript/blind75/arraysAndHashing.js
+++ javascript/blind75/arraysAndHashing.js
@@ -54,3 +54,13 @@
         return []
     };
 
+// Valid Palindrome
+    var isPalindrome = function(s) {
+        var newstr = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+        for(var i = 0, j = newstr.length-1; i <= j; i++, j--){
+            if(newstr[i] != newstr[j]){
+                return false
+            }
+        }
+        return true
+    };

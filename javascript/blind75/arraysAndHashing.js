6db0d5ebbb7a26d981d6995aaf919bac3761b591
davidpierce24 <pie2ce@gmail.com>
2022-08-16 10:49:44 -0400

    updates

--- javascript/blind75/arraysAndHashing.js
+++ javascript/blind75/arraysAndHashing.js
@@ -83,23 +83,3 @@
         }
         return profit
     };
-
-
-// Valid Parentheses
-    var isValid = function(s) {
-        if(s.length%2 != 0){
-            return false
-        }
-        var stack = []
-        
-        for(var i of s){
-            if(stack[stack.length-1] == '{' && i == '}' || stack[stack.length-1] == '(' && i == ')' || stack[stack.length-1] == '[' && i == ']'){
-                stack.pop()
-            }
-            else{
-                stack.push(i)
-            }
-        }
-            
-        return stack.length == 0
-    };

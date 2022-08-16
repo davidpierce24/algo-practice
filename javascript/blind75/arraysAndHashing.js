ce68b97df335296de87bec74c4332e8d1682636c
davidpierce24 <pie2ce@gmail.com>
2022-08-16 09:24:22 -0400

    valid anagram

--- javascript/blind75/arraysAndHashing.js
+++ javascript/blind75/arraysAndHashing.js
@@ -10,3 +10,22 @@
         }
         return false
     };
+
+// Valid Anagram
+    var isAnagram = function(s, t) {
+        if(s.length != t.length){
+            return false
+        }
+        var present  = 0
+        for(var i = t.length-1; i>= 0; i--){
+            for(var j of s){
+                if(t[i] == j){
+                    present += 1
+                }
+            }
+            if(present == 0){
+                return false
+            }
+        }
+        return true
+    };
